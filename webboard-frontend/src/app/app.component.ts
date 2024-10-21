0cffa9bfe74c001bcb15e0ba77fef5cdd1567d90
peem-teerawit <teerawit.madej@gmail.com>
2024-10-21 11:04:45 +0700

    update navbar
    remove thread-history, notification-button, and create-button when there is no token in local storage.

--- webboard-frontend/src/app/app.component.ts
+++ webboard-frontend/src/app/app.component.ts
@@ -1,30 +1,35 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router'; // Import Router
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webboard-frontend';
-
   username: string | null = '';
+  token: string | null = '';
   dropdownOpen = false;
 
-  constructor(private router: Router) { // Inject Router in the constructor
-    this.username = localStorage.getItem('username');
+  constructor(private router: Router) { }
+
+  // Lifecycle hook to check token and username
+  ngOnInit() {
+    this.token = localStorage.getItem('token'); // Check if token exists
+    this.username = localStorage.getItem('username'); // Get username if logged in
   }
 
   // Method to log out
   logOut() {
     localStorage.removeItem('token');
     localStorage.removeItem('username');
+    this.token = null; // Update token and username to null after logout
     this.username = null;
     this.closeDropdown();
 
     // Navigate to the thread page after logout
-    this.router.navigate(['/threads']); // Change '/threads' to the appropriate path if needed
+    this.router.navigate(['/threads']); // Change '/threads' if needed
   }
 
   toggleDropdown() {
